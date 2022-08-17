de9cc4bcb9b66de6a3fe19e124c3a97698c68cc8
gabrielcampos243 <gabrielcampos243@gmail.com>
2022-08-17 16:55:00 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+   get 'reviews/new'
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[new create]
+  end
+
+  root 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+mc = {name: 'mcburger', address: 'rua brito capelo', phone_number: '912334828', category: 'french'}
+pizza = {name: 'Pizza da pinta', address: 'rua brito cunha', phone_number: '933267829', category: 'belgian'}
+arroz = {name: 'Xauxau', address: 'rua do arroz', phone_number: '9872345672', category: 'chinese',}
+sushi = {name: 'sushiraria', address: 'Rua do suchi', phone_number: '923456782', category: 'japanese'}
+bagete = {name: 'casa das bagetes', address: 'Rua das bagetes', phone_number: '926272792', category: 'french'}
